d9b71b5f3a3d1c5ba25edcf54f755d16d154c414
bas0060 <adelekemakinde64@gmail.com>
2026-08-27 12:46:19 +0100

    add dynamic export to layout for improved rendering

--- app/(core)/layout.tsx
+++ app/(core)/layout.tsx
@@ -3,6 +3,8 @@ import { Navbar } from "@/components/layout/Navbar";
 import { getSessionUser } from "@/lib/auth";
 import { tagPostCounts } from "@/lib/db/queries";
 
+export const dynamic = "force-dynamic";
+
 export default async function CoreGroupLayout({
   children,
 }: {
